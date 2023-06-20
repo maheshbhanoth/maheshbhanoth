5493410d520fa24b01cc5905f04681a16574993b
MAHESH <MAHESH@LAPTOP-OLUGPO8H>
2023-06-20 19:47:14 +0530

    security-demo

---
+++ Downloads/feedback-demo/feedback-demo/src/main/java/com/example/feedbackdemo/controllers/FeedbackController.java
@@ -0,0 +1,5 @@
+package com.example.feedbackdemo.controllers;
+
+public class FeedbackController {
+
+}
---
+++ Downloads/feedback-demo/feedback-demo/src/test/java/com/example/feedbackdemo/FeedbackDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.feedbackdemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
